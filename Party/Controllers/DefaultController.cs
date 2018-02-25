0ddf859d49d713df636ba481e7c62177b5c2b476
Daron Short <daron.short@hotmail.com>
2018-02-25 11:26:02 -0600

    First commit and probably last

---
+++ Party/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using Party.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Party.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestresponse) {
+            if (ModelState.IsValid) {
+                return View("Thanks", guestresponse);
+            } else {
+                return View();
+            }
+        }
+    }
+}
---
+++ Party/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Party.Models {
+    public class GuestResponse {
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Phone Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Specify whether You'll Attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
